4b4535b02e3f17d26cd32c152aff4851d021ff6f
aruasouza <arua.viso@gmail.com>
2023-03-09 09:36:30 -0300

    changes

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from bcb import sgs
-import plotly.graph_objects as go
+from bcb import sgs,currency
+from datetime import date
+
+def absolute(serie):
+    valor_atual = 915.9300
+    yield valor_atual
+    for valor in serie[:-1]:
+        valor = valor / 100
+        valor_atual += (valor_atual * valor)
+        yield valor_atual
 
 def fetch_main_data():
-    data = {'Inflação':433}
-    df = sgs.get(data,start = '1994-08-01')
-    return df
+    data = {'Inflação':433,'PIB':22109,'Emprego Formal':28763,'SELIC':432}
+    for key in data:
+        if key not in st.session_state:
+            df = sgs.get({key:data[key]},start = '1994-08-01').dropna()
+            st.session_state[key] = df
+    if 'USD' not in st.session_state:
+        cy = currency.get('USD', start = '1994-08-01',end = str(date.today())).dropna()
+        st.session_state['USD'] = cy
 
-df = fetch_main_data()
+def enrich_data():
+    if 'Inflação Acumulada' not in st.session_state:
+        df = st.session_state['Inflação'].copy()
+        df['Inflação Acumulada'] = [valor for valor in absolute(df['Inflação'].values)]
+        st.session_state['Inflação Acumulada'] = df[['Inflação Acumulada']].copy()
+    
+
+fetch_main_data()
+enrich_data()
 
 st.set_page_config(page_title = 'Economia Brasileira',layout = 'wide',page_icon = '&#128200')
 style = '''
@@ -21,8 +42,9 @@ header {visibility:hidden;}
 .e1fqkh3o4 {padding-top:1rem;}
 </style>
 '''
-
 st.markdown(style,unsafe_allow_html=True)
 
-st.title('Inflação')
-st.line_chart(df)
+indices = [key for key in st.session_state]
+tabs = st.tabs(indices)
+for indice,tab in zip(indices,tabs):
+    tab.line_chart(st.session_state[indice])
